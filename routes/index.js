e58c27a2f4e331af6844ca0d18580b27f9d37146
Fadi Bitar <Fadi@Fadis-MacBook-Pro.local>
2016-04-05 18:51:58 -0400

    updated files

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Fadi Bitar' });
+  res.render('index', { title: 'JADE-bootstrap' });
 });
 
 module.exports = router;
